5916264f10820058c9f2bb2b71463588cd3fd270
nyoman warsana <inyomanwarsana1@gmail.com>
2024-11-26 07:18:33 +0800

    Refactor : move the interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import genre from "../data/genre";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
 const useGenres = () =>
   useQuery<FetchResponse<Genre>>({
     queryKey: ["genres"],
