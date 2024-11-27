d3eebfec19fa3839a9dba45264db9fe0d86ee174
nyoman warsana <inyomanwarsana1@gmail.com>
2024-11-27 09:54:25 +0800

    Refactoring entities

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import genre from "../data/genre";
-import { Genre } from "../entities/Genre";
+import Genre from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
 const useGenres = () =>
