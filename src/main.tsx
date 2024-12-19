5ff0c49973955bf9a10a841a1a207a0184f01f2b
warsana <inyomanwarsana1@gmail.com>
2024-12-19 18:44:23 +0800

    changing the title

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import theme from "./theme";
 import router from "./routes";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
